fdee28a8df30cfd0c95d1e52f584cddbda20589b
Khai Phan Van <phan_van_khai123@yahoo.com>
2021-03-03 09:43:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state: {
+      latitude: null
+    };
+  }
+
+  //React says we have to define render
+  render() {
+    window.navigation.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+    return <div>Latitude:</div>
+  }
 }
 
-export default App;
+ReactDOM.render(<App />, document.querySelector('#root'));
